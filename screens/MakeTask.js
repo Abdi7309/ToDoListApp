eafd730a724592d4b47e9b3c2679144ab1bda55d
Abdi7309 <512875@vistacollege.nl>
2025-02-07 08:51:45 +0100

    0.1%

--- screens/MakeTask.js
+++ screens/MakeTask.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import { View, Text, Image, SafeAreaView, TextInput, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, Image, SafeAreaView, TextInput, TouchableOpacity } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import RNPickerSelect from 'react-native-picker-select'; 
-import styles from './uiterlijk2';
 
 class MakeTask extends React.Component {
   constructor(props) {
@@ -109,5 +108,103 @@ class MakeTask extends React.Component {
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'rgba(245, 245, 245, 1)',
+    flex: 1,
+  },
+  terug: {
+    width: 25,
+    height: 25,
+    left: 350,
+    top: 20,
+  },
+  TaskText: {
+    color: 'black',
+    top: 50,
+    left: 150,
+    fontSize: 25,
+    fontWeight: '600',
+  },
+  planText: {
+    color: 'rgba(169, 169, 169, 1)',
+    top: 95,
+    left: 30,
+    fontSize: 13,
+  },
+  input: {
+    height: 90,
+    borderWidth: 0,
+    top: 95,
+    fontSize: 40,
+    borderColor: 'gray',
+    margin: 10,
+    padding: 8,
+    borderBottomWidth: 1,  
+  },
+  input2: {
+    height: 40,
+    borderWidth: 0,
+    top: 120,
+    marginLeft: 100,
+    marginRight: 100,
+    margin: 10,
+    padding: 8,
+    fontSize: 16,
+  },
+  input2Placeholder: {
+    color: 'black',
+  },
+  icon1: {
+    top: 164, 
+    marginLeft: 50,
+    height: 30,
+    width: 30,
+  }, 
+  saveButton: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0, 
+    right: 0,
+    height: 65,
+    backgroundColor: 'rgba(49, 74, 164, 1)',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  pickerSelectContainer: {
+    height: 40,
+    width: 215,
+    top: 120,
+    left: 91,
+  },
+  pickerSelectText: {
+    fontSize: 14,
+  },
+  pickerSelectAndroid: {
+    fontSize: 14,
+    paddingHorizontal: 10,
+    paddingVertical: 8,
+    borderWidth: 0.5,
+    borderColor: 'purple',
+    borderRadius: 8,
+    color: 'black',
+    paddingRight: 30, 
+  },
+  pickerSelectIOS: {
+    fontSize: 14,
+    paddingVertical: 12,
+    paddingHorizontal: 10,
+    borderWidth: 1,
+    borderColor: 'gray',
+    borderRadius: 4,
+    color: 'black',
+    paddingRight: 30, 
+  },
+  pickerSelectPlaceholder: {
+    fontSize: 14,
+    color: 'black',
+  },
+});
 
 export default MakeTask;
